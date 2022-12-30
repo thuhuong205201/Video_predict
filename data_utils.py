fc20f0279fcd880d392f669cb469a013cb37ad13
Thu Hương <74065603+thuhuong205201@users.noreply.github.com>
2022-12-30 11:28:17 +0700

    Add files via upload

---
+++ data_utils.py
@@ -0,0 +1,522 @@
+import math
+import torch
+import socket
+import argparse
+import os
+import numpy as np
+from sklearn.manifold import TSNE
+import scipy.misc
+import matplotlib
+
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import functools
+from skimage.metrics import peak_signal_noise_ratio as psnr_metric
+from skimage.metrics import structural_similarity as ssim_metric
+
+from scipy import signal
+from scipy import ndimage
+from PIL import Image, ImageDraw
+
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import imageio
+
+hostname = socket.gethostname()
+
+_errstr = "Mode is unknown or incompatible with input array shape."
+
+
+def bytescale(data, cmin=None, cmax=None, high=255, low=0):
+    """
+    Byte scales an array (image).
+    Byte scaling means converting the input image to uint8 dtype and scaling
+    the range to ``(low, high)`` (default 0-255).
+    If the input image already has dtype uint8, no scaling is done.
+    This function is only available if Python Imaging Library (PIL) is installed.
+    Parameters
+    ----------
+    data : ndarray
+        PIL image data array.
+    cmin : scalar, optional
+        Bias scaling of small values. Default is ``data.min()``.
+    cmax : scalar, optional
+        Bias scaling of large values. Default is ``data.max()``.
+    high : scalar, optional
+        Scale max value to `high`.  Default is 255.
+    low : scalar, optional
+        Scale min value to `low`.  Default is 0.
+    Returns
+    -------
+    img_array : uint8 ndarray
+        The byte-scaled array.
+    Examples
+    --------
+    >>> from scipy.misc import bytescale
+    >>> img = np.array([[ 91.06794177,   3.39058326,  84.4221549 ],
+    ...                 [ 73.88003259,  80.91433048,   4.88878881],
+    ...                 [ 51.53875334,  34.45808177,  27.5873488 ]])
+    >>> bytescale(img)
+    array([[255,   0, 236],
+           [205, 225,   4],
+           [140,  90,  70]], dtype=uint8)
+    >>> bytescale(img, high=200, low=100)
+    array([[200, 100, 192],
+           [180, 188, 102],
+           [155, 135, 128]], dtype=uint8)
+    >>> bytescale(img, cmin=0, cmax=255)
+    array([[91,  3, 84],
+           [74, 81,  5],
+           [52, 34, 28]], dtype=uint8)
+    """
+    if data.dtype == np.uint8:
+        return data
+
+    if high > 255:
+        raise ValueError("`high` should be less than or equal to 255.")
+    if low < 0:
+        raise ValueError("`low` should be greater than or equal to 0.")
+    if high < low:
+        raise ValueError("`high` should be greater than or equal to `low`.")
+
+    if cmin is None:
+        cmin = data.min()
+    if cmax is None:
+        cmax = data.max()
+
+    cscale = cmax - cmin
+    if cscale < 0:
+        raise ValueError("`cmax` should be larger than `cmin`.")
+    elif cscale == 0:
+        cscale = 1
+
+    scale = float(high - low) / cscale
+    bytedata = (data - cmin) * scale + low
+    return (bytedata.clip(low, high) + 0.5).astype(np.uint8)
+
+
+def toimage(arr, high=255, low=0, cmin=None, cmax=None, pal=None,
+            mode=None, channel_axis=None):
+    """Takes a numpy array and returns a PIL image.
+    This function is only available if Python Imaging Library (PIL) is installed.
+    The mode of the PIL image depends on the array shape and the `pal` and
+    `mode` keywords.
+    For 2-D arrays, if `pal` is a valid (N,3) byte-array giving the RGB values
+    (from 0 to 255) then ``mode='P'``, otherwise ``mode='L'``, unless mode
+    is given as 'F' or 'I' in which case a float and/or integer array is made.
+    .. warning::
+        This function uses `bytescale` under the hood to rescale images to use
+        the full (0, 255) range if ``mode`` is one of ``None, 'L', 'P', 'l'``.
+        It will also cast data for 2-D images to ``uint32`` for ``mode=None``
+        (which is the default).
+    Notes
+    -----
+    For 3-D arrays, the `channel_axis` argument tells which dimension of the
+    array holds the channel data.
+    For 3-D arrays if one of the dimensions is 3, the mode is 'RGB'
+    by default or 'YCbCr' if selected.
+    The numpy array must be either 2 dimensional or 3 dimensional.
+    """
+    data = np.asarray(arr)
+    if np.iscomplexobj(data):
+        raise ValueError("Cannot convert a complex-valued array.")
+    shape = list(data.shape)
+    valid = len(shape) == 2 or ((len(shape) == 3) and
+                                ((3 in shape) or (4 in shape)))
+    if not valid:
+        raise ValueError("'arr' does not have a suitable array shape for "
+                         "any mode.")
+    if len(shape) == 2:
+        shape = (shape[1], shape[0])  # columns show up first
+        if mode == 'F':
+            data32 = data.astype(np.float32)
+            image = Image.frombytes(mode, shape, data32.tostring())
+            return image
+        if mode in [None, 'L', 'P']:
+            bytedata = bytescale(data, high=high, low=low,
+                                 cmin=cmin, cmax=cmax)
+            image = Image.frombytes('L', shape, bytedata.tostring())
+            if pal is not None:
+                image.putpalette(np.asarray(pal, dtype=np.uint8).tostring())
+                # Becomes a mode='P' automagically.
+            elif mode == 'P':  # default gray-scale
+                pal = (np.arange(0, 256, 1, dtype=np.uint8)[:, np.newaxis] *
+                       np.ones((3,), dtype=np.uint8)[np.newaxis, :])
+                image.putpalette(np.asarray(pal, dtype=np.uint8).tostring())
+            return image
+        if mode == '1':  # high input gives threshold for 1
+            bytedata = (data > high)
+            image = Image.frombytes('1', shape, bytedata.tostring())
+            return image
+        if cmin is None:
+            cmin = np.amin(np.ravel(data))
+        if cmax is None:
+            cmax = np.amax(np.ravel(data))
+        data = (data*1.0 - cmin)*(high - low)/(cmax - cmin) + low
+        if mode == 'I':
+            data32 = data.astype(np.uint32)
+            image = Image.frombytes(mode, shape, data32.tostring())
+        else:
+            raise ValueError(_errstr)
+        return image
+
+    # if here then 3-d array with a 3 or a 4 in the shape length.
+    # Check for 3 in datacube shape --- 'RGB' or 'YCbCr'
+    if channel_axis is None:
+        if (3 in shape):
+            ca = np.flatnonzero(np.asarray(shape) == 3)[0]
+        else:
+            ca = np.flatnonzero(np.asarray(shape) == 4)
+            if len(ca):
+                ca = ca[0]
+            else:
+                raise ValueError("Could not find channel dimension.")
+    else:
+        ca = channel_axis
+
+    numch = shape[ca]
+    if numch not in [3, 4]:
+        raise ValueError("Channel axis dimension is not valid.")
+
+    bytedata = bytescale(data, high=high, low=low, cmin=cmin, cmax=cmax)
+    if ca == 2:
+        strdata = bytedata.tostring()
+        shape = (shape[1], shape[0])
+    elif ca == 1:
+        strdata = np.transpose(bytedata, (0, 2, 1)).tostring()
+        shape = (shape[2], shape[0])
+    elif ca == 0:
+        strdata = np.transpose(bytedata, (1, 2, 0)).tostring()
+        shape = (shape[2], shape[1])
+    if mode is None:
+        if numch == 3:
+            mode = 'RGB'
+        else:
+            mode = 'RGBA'
+
+    if mode not in ['RGB', 'RGBA', 'YCbCr', 'CMYK']:
+        raise ValueError(_errstr)
+
+    if mode in ['RGB', 'YCbCr']:
+        if numch != 3:
+            raise ValueError("Invalid array shape for mode.")
+    if mode in ['RGBA', 'CMYK']:
+        if numch != 4:
+            raise ValueError("Invalid array shape for mode.")
+
+    # Here we know data and mode is correct
+    image = Image.frombytes(mode, shape, strdata)
+    return image
+
+def load_dataset(opt):
+    if opt.dataset == 'smmnist':
+        from data.moving_mnist import MovingMNIST
+        train_data = MovingMNIST(
+            train=True,
+            data_root=opt.data_root,
+            seq_len=opt.max_step,
+            image_size=opt.image_width,
+            num_digits=opt.num_digits)
+        test_data = MovingMNIST(
+            train=False,
+            data_root=opt.data_root,
+            seq_len=opt.max_step,
+            image_size=opt.image_width,
+            num_digits=opt.num_digits)
+    elif opt.dataset == 'bair':
+        from data.bair import RobotPush
+        train_data = RobotPush(
+            data_root=opt.data_root,
+            train=True,
+            seq_len=opt.max_step,
+            image_size=opt.image_width)
+        test_data = RobotPush(
+            data_root=opt.data_root,
+            train=False,
+            seq_len=opt.n_eval,
+            image_size=opt.image_width)
+    elif opt.dataset == 'kth':
+        train_data = KTH(
+                train=True,
+                data_root=opt.data_root,
+                seq_len=opt.max_step,
+                image_size=opt.image_width,
+                data_type=opt.data_type)
+        test_data = KTH(
+                train=False,
+                data_root=opt.data_root,
+                seq_len=opt.max_step,
+                image_size=opt.image_width,
+                data_type=opt.data_type)
+    elif opt.dataset == 'ucf':
+        train_data = UCF(
+                data_root=opt.data_root,
+                seq_len=opt.max_step,
+                train=True
+                )
+        test_data =UCF(
+                data_root=opt.data_root,
+                seq_len=opt.max_step,
+                train=False
+                )
+    elif opt.dataset == 'kitti':
+        from data.kitti import KITTI
+        train_data = KITTI(
+            train=True,
+            data_root=opt.data_root,
+            seq_len=opt.max_step)
+        test_data = KITTI(
+            train=False,
+            data_root=opt.data_root,
+            seq_len=opt.max_step)
+
+    return train_data, test_data
+
+
+def sequence_input(seq, dtype):
+    return [Variable(x.type(dtype)) for x in seq]
+
+
+def normalize_data(opt, dtype, sequence):
+    if opt.dataset == 'smmnist' or opt.dataset == 'smmnist2' or opt.dataset == 'kth' or opt.dataset == 'bair' or opt.dataset == 'ucf' or opt.dataset == 'kitti':
+        sequence.transpose_(0, 1)
+        sequence.transpose_(3, 4).transpose_(2, 3)
+    else:
+        sequence.transpose_(0, 1)
+
+    return sequence_input(sequence, dtype)
+
+
+def is_sequence(arg):
+    return (not hasattr(arg, "strip") and
+            not type(arg) is np.ndarray and
+            not hasattr(arg, "dot") and
+            (hasattr(arg, "__getitem__") or
+             hasattr(arg, "__iter__")))
+
+
+def image_tensor(inputs, padding=1):
+    # assert is_sequence(inputs)
+    assert len(inputs) > 0
+    # print(inputs)
+
+    # if this is a list of lists, unpack them all and grid them up
+    if is_sequence(inputs[0]) or (hasattr(inputs, "dim") and inputs.dim() > 4):
+        images = [image_tensor(x) for x in inputs]
+        if images[0].dim() == 3:
+            c_dim = images[0].size(0)
+            x_dim = images[0].size(1)
+            y_dim = images[0].size(2)
+        else:
+            c_dim = 1
+            x_dim = images[0].size(0)
+            y_dim = images[0].size(1)
+
+        result = torch.ones(c_dim,
+                            x_dim * len(images) + padding * (len(images) - 1),
+                            y_dim)
+        for i, image in enumerate(images):
+            result[:, i * x_dim + i * padding:
+                      (i + 1) * x_dim + i * padding, :].copy_(image)
+
+        return result
+
+    # if this is just a list, make a stacked image
+    else:
+        images = [x.data if isinstance(x, torch.autograd.Variable) else x
+                  for x in inputs]
+        # print(images)
+        if images[0].dim() == 3:
+            c_dim = images[0].size(0)
+            x_dim = images[0].size(1)
+            y_dim = images[0].size(2)
+        else:
+            c_dim = 1
+            x_dim = images[0].size(0)
+            y_dim = images[0].size(1)
+
+        result = torch.ones(c_dim,
+                            x_dim,
+                            y_dim * len(images) + padding * (len(images) - 1))
+        for i, image in enumerate(images):
+            result[:, :, i * y_dim + i * padding:
+                         (i + 1) * y_dim + i * padding].copy_(image)
+        return result
+
+
+def save_np_img(fname, x):
+    if x.shape[0] == 1:
+        x = np.tile(x, (3, 1, 1))
+    img = toimage(x,
+                             high=255.,
+                             channel_axis=0)
+    img.save(fname)
+
+
+def make_image(tensor):
+    tensor = tensor.cpu().clamp(0, 1)
+    if tensor.size(0) == 1:
+        tensor = tensor.expand(3, tensor.size(1), tensor.size(2))
+    # pdb.set_trace()
+    return toimage(tensor.numpy(),
+                              high=255.,
+                              channel_axis=0)
+
+
+def draw_text_tensor(tensor, text):
+    np_x = tensor.transpose(0, 1).transpose(1, 2).data.cpu().numpy()
+    pil = Image.fromarray(np.uint8(np_x * 255))
+    draw = ImageDraw.Draw(pil)
+    draw.text((4, 64), text, (0, 0, 0))
+    img = np.asarray(pil)
+    return Variable(torch.Tensor(img / 255.)).transpose(1, 2).transpose(0, 1)
+
+
+def save_gif(filename, inputs, duration=0.25):
+    images = []
+    for tensor in inputs:
+        img = image_tensor(tensor, padding=0)
+        img = img.cpu()
+        img = img.transpose(0, 1).transpose(1, 2).clamp(0, 1)
+        images.append(img.numpy())
+    imageio.mimsave(filename, images, duration=duration)
+
+
+def save_gif_with_text(filename, inputs, text, duration=0.25):
+    images = []
+    for tensor, text in zip(inputs, text):
+        img = image_tensor([draw_text_tensor(ti, texti) for ti, texti in zip(tensor, text)], padding=0)
+        img = img.cpu()
+        img = img.transpose(0, 1).transpose(1, 2).clamp(0, 1).numpy()
+        images.append(img)
+    imageio.mimsave(filename, images, duration=duration)
+
+
+def save_image(filename, tensor):
+    img = make_image(tensor)
+    img.save(filename)
+
+
+def save_tensors_image(filename, inputs, padding=1):
+    images = image_tensor(inputs, padding)
+    return save_image(filename, images)
+
+
+def prod(l):
+    return functools.reduce(lambda x, y: x * y, l)
+
+
+def batch_flatten(x):
+    return x.resize(x.size(0), prod(x.size()[1:]))
+
+
+def clear_progressbar():
+    # moves up 3 lines
+    print("\033[2A")
+    # deletes the whole line, regardless of character position
+    print("\033[2K")
+    # moves up two lines again
+    print("\033[2A")
+
+
+def mse_metric(x1, x2):
+    err = np.sum((x1 - x2) ** 2)
+    err /= float(x1.shape[0] * x1.shape[1] * x1.shape[2])
+    return err
+
+
+def eval_seq(gt, pred):
+    T = len(gt)
+    bs = gt[0].shape[0]
+    ssim = np.zeros((bs, T))
+    psnr = np.zeros((bs, T))
+    mse = np.zeros((bs, T))
+    for i in range(bs):
+        for t in range(T):
+            for c in range(gt[t][i].shape[0]):
+                ssim[i, t] += ssim_metric(gt[t][i][c], pred[t][i][c])
+                psnr[i, t] += psnr_metric(gt[t][i][c], pred[t][i][c])
+            ssim[i, t] /= gt[t][i].shape[0]
+            psnr[i, t] /= gt[t][i].shape[0]
+            mse[i, t] = mse_metric(gt[t][i], pred[t][i])
+
+    return mse, ssim, psnr
+
+
+# ssim function used in Babaeizadeh et al. (2017), Fin et al. (2016), etc.
+def finn_eval_seq(gt, pred):
+    T = len(gt)
+    bs = gt[0].shape[0]
+    ssim = np.zeros((bs, T))
+    psnr = np.zeros((bs, T))
+    mse = np.zeros((bs, T))
+    for i in range(bs):
+        for t in range(T):
+            for c in range(gt[t][i].shape[0]):
+                res = finn_ssim(gt[t][i][c], pred[t][i][c]).mean()
+                if math.isnan(res):
+                    ssim[i, t] += -1
+                else:
+                    ssim[i, t] += res
+                psnr[i, t] += finn_psnr(gt[t][i][c], pred[t][i][c])
+            ssim[i, t] /= gt[t][i].shape[0]
+            psnr[i, t] /= gt[t][i].shape[0]
+            mse[i, t] = mse_metric(gt[t][i], pred[t][i])
+
+    return mse, ssim, psnr
+
+
+def finn_psnr(x, y):
+    mse = ((x - y) ** 2).mean()
+    return 10 * np.log(1 / mse) / np.log(10)
+
+
+def gaussian2(size, sigma):
+    A = 1 / (2.0 * np.pi * sigma ** 2)
+    x, y = np.mgrid[-size // 2 + 1:size // 2 + 1, -size // 2 + 1:size // 2 + 1]
+    g = A * np.exp(-((x ** 2 / (2.0 * sigma ** 2)) + (y ** 2 / (2.0 * sigma ** 2))))
+    return g
+
+
+def fspecial_gauss(size, sigma):
+    x, y = np.mgrid[-size // 2 + 1:size // 2 + 1, -size // 2 + 1:size // 2 + 1]
+    g = np.exp(-((x ** 2 + y ** 2) / (2.0 * sigma ** 2)))
+    return g / g.sum()
+
+
+def finn_ssim(img1, img2, cs_map=False):
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    size = 11
+    sigma = 1.5
+    window = fspecial_gauss(size, sigma)
+    K1 = 0.01
+    K2 = 0.03
+    L = 1  # bitdepth of image
+    C1 = (K1 * L) ** 2
+    C2 = (K2 * L) ** 2
+    mu1 = signal.fftconvolve(img1, window, mode='valid')
+    mu2 = signal.fftconvolve(img2, window, mode='valid')
+    mu1_sq = mu1 * mu1
+    mu2_sq = mu2 * mu2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = signal.fftconvolve(img1 * img1, window, mode='valid') - mu1_sq
+    sigma2_sq = signal.fftconvolve(img2 * img2, window, mode='valid') - mu2_sq
+    sigma12 = signal.fftconvolve(img1 * img2, window, mode='valid') - mu1_mu2
+    if cs_map:
+        return (((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                             (sigma1_sq + sigma2_sq + C2)),
+                (2.0 * sigma12 + C2) / (sigma1_sq + sigma2_sq + C2))
+    else:
+        return ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+
+
+def init_weights(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1 or classname.find('Linear') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+        m.bias.data.fill_(0)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
